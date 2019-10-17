ddfdcbc38dfa9da463d7c635ed9810d583e03e99
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 01:36:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+puts "hi"
+2+4
 end
 
 def subtraction(num1, num2)
-
+puts "Hello"
+3-4
 end
 
 def division(num1, num2)
-
+puts "Hello"
+3/4
 end
 
 def multiplication(num1, num2)
-
+puts "multi"
+3*4
 end
 
 def modulo(num1, num2)
-
+puts "modu"
+math.sin(30)
 end
 
 def square_root(num)
-
+puts "square"
+math.sqrt 4
 end
